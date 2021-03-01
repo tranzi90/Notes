f1fd3c7b9c9c1901b3342668632ebe73a23a34d4
denis <tranzi90@gmail.com>
2021-03-02 00:09:37 +0300

    prettify code

--- app.js
+++ app.js
@@ -1,64 +1,55 @@
-const fs = require('fs');
-const _ = require('lodash');
-const yargs = require('yargs');
+const fs = require('fs')
+const _ = require('lodash')
+const yargs = require('yargs')
 
-const notes = require('./notes.js');
+const notes = require('./notes.js')
 
 const title = {
     describe: 'Title of note',
     demand: true,
-    alias: 't'
-};
+    alias: 't',
+}
 
 const body = {
     describe: 'Body of note',
     demand: true,
-    alias: 'b'
-};
+    alias: 'b',
+}
 
 const argv = yargs
     .command('add', 'Add a new note', {
-    title,
-    body
+        title,
+        body,
     })
     .command('list', 'List all notes')
     .command('read', 'Read a note', {
-        title
+        title,
     })
     .command('remove', 'Remove a note', {
-        title
+        title,
     })
-    .help()
-    .argv;
+    .help().argv
 
-var command = argv._[0];
+var command = argv._[0]
 
 if (command === 'add') {
-    var note = notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body)
     if (note) {
-        console.log(`Note added!`);
-        notes.logNote(note);
-    }
-    else
-        console.log(`Note ${argv.title} already exists!`);
-}
-else if (command === 'list') {
-    var allNotes = notes.getAll();
-    console.log(`Total ${allNotes.length} notes:`);
-    allNotes.forEach((note) => notes.logNote(note));
-}
-else if (command === 'read') {
-    var noteTaken = notes.getNote(argv.title);
-    if (noteTaken)
-        notes.logNote(noteTaken);
-    else
-        console.log(`Note ${argv.title} not found!`);
-}
-else if (command === 'remove') {
-    var noteRemoved = notes.removeNote(argv.title);
-    var message = noteRemoved ? 'Заметка удалена' : 'не удалено';
-    console.log(message);
-}
-else {
-    console.log('Wrong command');
+        console.log(`Note added!`)
+        notes.logNote(note)
+    } else console.log(`Note ${argv.title} already exists!`)
+} else if (command === 'list') {
+    var allNotes = notes.getAll()
+    console.log(`Total ${allNotes.length} notes:`)
+    allNotes.forEach((note) => notes.logNote(note))
+} else if (command === 'read') {
+    var noteTaken = notes.getNote(argv.title)
+    if (noteTaken) notes.logNote(noteTaken)
+    else console.log(`Note ${argv.title} not found!`)
+} else if (command === 'remove') {
+    var noteRemoved = notes.removeNote(argv.title)
+    var message = noteRemoved ? 'Заметка удалена' : 'не удалено'
+    console.log(message)
+} else {
+    console.log('Wrong command')
 }
--- notes.js
+++ notes.js
@@ -1,57 +1,56 @@
-const fs = require('fs');
+const fs = require('fs')
 
 var fetchNotes = () => {
     try {
-        var notesStr = fs.readFileSync('notes-data.json');
-        return JSON.parse(notesStr);
+        var notesStr = fs.readFileSync('notes-data.json')
+        return JSON.parse(notesStr)
+    } catch (e) {
+        return []
     }
-    catch (e) {
-        return [];
-    }
-};
+}
 
 var saveNotes = (notes) => {
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-};
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+}
 
 var addNote = (title, body) => {
-    var notes = fetchNotes();
+    var notes = fetchNotes()
     var note = {
         title,
-        body
-    };
+        body,
+    }
 
-    var duplicates = notes.filter((note) => note.title === title);
+    var duplicates = notes.filter((note) => note.title === title)
 
     if (duplicates.length === 0) {
-        notes.push(note);
-        saveNotes(notes);
-        return note;
+        notes.push(note)
+        saveNotes(notes)
+        return note
     }
 }
 
 var getAll = () => {
-    return fetchNotes();
+    return fetchNotes()
 }
 
 var getNote = (title) => {
-    var notes = fetchNotes();
-    var ourNote = notes.filter((note) => note.title === title);
-    return ourNote[0];
+    var notes = fetchNotes()
+    var ourNote = notes.filter((note) => note.title === title)
+    return ourNote[0]
 }
 
 var removeNote = (title) => {
-    var notes = fetchNotes();
-    var newNotes = notes.filter((note) => note.title !== title);
-    saveNotes(newNotes);
+    var notes = fetchNotes()
+    var newNotes = notes.filter((note) => note.title !== title)
+    saveNotes(newNotes)
 
-    return notes.length !== newNotes.length;
+    return notes.length !== newNotes.length
 }
 
 var logNote = (note) => {
-    console.log('------------------');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    console.log('------------------')
+    console.log(`Title: ${note.title}`)
+    console.log(`Body: ${note.body}`)
 }
 
 module.exports = {
@@ -59,5 +58,5 @@ module.exports = {
     getAll,
     getNote,
     removeNote,
-    logNote
-};
+    logNote,
+}
